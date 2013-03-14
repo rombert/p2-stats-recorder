9ccfcb20691aa1a3c7df998b3b27b20b6fb50089
Robert Munteanu <robert@lmn.ro>
2013-03-14 23:06:40 +0200

    Also track host addresses of clients
    Affects #15655: Record installation statistics

--- index.php
+++ index.php
@@ -7,11 +7,12 @@ $iu = isset($_SERVER['PATH_INFO']) ? substr($_SERVER['PATH_INFO'], 1) : '';
 $package = isset( $_REQUEST['package'] ) ? $_REQUEST['package'] : '';
 $version = isset( $_REQUEST['version']) ? $_REQUEST['version'] : '';
 $os = isset ( $_REQUEST['os'] ) ? $_REQUEST['os'] : '';
+$host = isset ($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 
 if ( strlen($iu) > 0 ) {
 	$pdo = new PDO(PDO_DSN, PDO_USERNAME, PDO_PASSWORD);
-	$statement = $pdo->prepare('INSERT INTO installations(iu, package, version, os) VALUES(?, ?, ?, ?)');
-	$statement->execute(array($iu, $package, $version, $os));
+	$statement = $pdo->prepare('INSERT INTO installations(iu, package, version, os, host) VALUES(?, ?, ?, ?, ?)');
+	$statement->execute(array($iu, $package, $version, $os, $host));
 }
 
 // file_put_contents('/tmp/log_install', "$bundle,$package,$version,$os");
--- schema.sql
+++ schema.sql
@@ -3,5 +3,6 @@ CREATE TABLE installations (
    package   tinytext,
    os        tinytext,
    version   tinytext,
+   host		tinytext,
    tstamp    timestamp DEFAULT "CURRENT_TIMESTAMP"
 )
